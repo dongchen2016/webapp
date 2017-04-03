1f93f9affaa179d7beab80f9bdf92f6e68260266
马冬冬 <madongdong@ofpay.com>
2017-04-03 15:50:36 +0800

    2

---
+++ src/main/java/com/demo/persistence/TestMapper.java
@@ -0,0 +1,13 @@
+package com.demo.persistence;
+
+import com.demo.entity.TbUser;
+
+import java.util.List;
+
+/**
+ * Created by admin on 2017/04/03.
+ */
+public interface TestMapper {
+
+    List<TbUser> getAllUsers();
+}
