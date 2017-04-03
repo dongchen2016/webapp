4d7a273f502881fde7c22ebaf69871f88c3b1c45
马冬冬 <madongdong@ofpay.com>
2017-04-03 15:47:40 +0800

    1

--- src/main/java/com/demo/web/action/HelloController.java
+++ src/main/java/com/demo/web/action/HelloController.java
@@ -1,16 +1,24 @@
 package com.demo.web.action;
 
 
+import com.demo.entity.TbUser;
 import com.demo.entity.UserInfo;
+import com.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by admin on 2017/03/31.
  */
 @Controller
 public class HelloController {
+
+    @Autowired
+    private TestService testService;
 
     @RequestMapping(value = "/hello" ,method = {RequestMethod.GET,RequestMethod.POST})
     public String handleRequest(Model model){
@@ -29,6 +37,7 @@ public class HelloController {
     public String pathVariable(Model model, @ModelAttribute UserInfo userinfo, @PathVariable String message){
         model.addAttribute("username",userinfo.getUsername());
         model.addAttribute("username",userinfo.getPassword());
+        List<TbUser> users = testService.getAllUser();
         return "welcome";
     }
 
