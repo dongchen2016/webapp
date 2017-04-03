15b98e74dbdc2a17df337f264c7aaf14baf3f0bf
马冬冬 <madongdong@ofpay.com>
2017-04-03 15:27:12 +0800

    Mybatis基础配置，

--- src/main/java/com/demo/web/action/HelloController.java
+++ src/main/java/com/demo/web/action/HelloController.java
@@ -1,7 +1,7 @@
 package com.demo.web.action;
 
 
-import com.demo.web.entity.UserInfo;
+import com.demo.entity.UserInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
